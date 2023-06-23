68824e3c06f806939918364dedcefb6b8bf22cce
ec2-user <ec2-user@Mac-mini.local>
2023-06-23 11:59:28 +0000

    2.1.0

--- Package.swift
+++ Package.swift
@@ -3,15 +3,13 @@
 
 import PackageDescription
 
-let package = Package(name: "LRAtsSDK",
+let package = Package(name: "LRAts",
                       products: [
-                          // Products define the executables and libraries a package produces, and make them visible to other packages.
-                          .library(
-                                  name: "LRAtsSDK",
-                                  targets: ["LRAtsSDK"]),
+                        .library(
+                            name: "LRAtsSDK",
+                            targets: ["LRAtsSDK"])
                       ],
-                      dependencies: [],
                       targets: [
-                          .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.0.0/LRAtsSDK.zip", checksum: "f72742f198318eb1f440713626a71e50213fb4e942922bcd48633bae5ec2ac4d")
+                        .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.1.0/LRAtsSDK.zip", checksum: "924091dbe02f6509a714e429fda44a6da57d06e97f39c8dafef5279d77d416fa")
                       ]
 )
