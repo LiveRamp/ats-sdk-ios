b7e1249d3a7d6fc58199bb70a9505013d1264a5a
LiveRamp Mobile <mobilesdk@liveramp.com>
2025-01-23 15:45:20 +0000

    2.9.0

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(name: "LRAts",
                             targets: ["LRAtsSDK"])
                       ],
                       targets: [
-                        .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.8.0/LRAtsSDK.zip", checksum: "4b75b614e20f83dd35b6bed8c7421944b4463f0ac782c6bdfe856af3982572cb")
+                        .binaryTarget(name: "LRAtsSDK", url: "https://ats-sdk-ios-prod.launch.liveramp.com/2.9.0/LRAtsSDK.zip", checksum: "93ba3b579ed2fee46fee47c616c962fab140b7d0ce912aa92d4b5b543881f3f2")
                       ]
 )
